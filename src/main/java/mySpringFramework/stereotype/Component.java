9dce5b13870a9302e43e344189dacb2d5bf1d6f2
ww1346 <wmr1346@bupt.edu.cn>
2022-02-18 00:12:01 +0800

    main-AoP

---
+++ src/main/java/mySpringFramework/stereotype/Component.java
@@ -0,0 +1,13 @@
+package mySpringFramework.stereotype;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Component {
+
+    String value() default "";
+
+}
+
